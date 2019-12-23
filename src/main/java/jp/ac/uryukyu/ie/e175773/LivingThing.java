1afe5729d1d308f57898abe016804136d3905978
Shoma KYAN <e175773@ie.u-ryukyu.ac.jp>
2019-12-23 23:32:03 +0900

    step3

--- src/main/java/jp/ac/uryukyu/ie/e175773/LivingThing.java
+++ src/main/java/jp/ac/uryukyu/ie/e175773/LivingThing.java
@@ -6,6 +6,13 @@ public class LivingThing {
     private int attack;
     private boolean dead;
 
+    /**
+     *コンストラクタ
+     * @param name:名前
+     * @param maximumHP:最大HP
+     * @param attack:攻撃力
+     */
+
     public LivingThing(String name, int maximumHP, int attack) {
         this.name = name;
         hitPoint = maximumHP;
@@ -14,13 +21,28 @@ public class LivingThing {
         System.out.printf("%sのHPは%d。攻撃力は%dです。\n", name, maximumHP, attack);
     }
 
+    /**
+     * 死んでいるのかを判定するメソッド
+     * @return boolean型,Trueなら死亡判定
+     */
+
     public boolean isDead() {
         return dead;
     }
 
+    /**
+     * 名前を戻り値とするゲッターメソッド
+     * @return:名前
+     */
+
     public String getName() {
         return name;
     }
+
+    /**
+     * 敵を攻撃してダメージを与えるattackメソッド
+     * @param opponent:相手キャラ
+     */
 
     public void attack(LivingThing opponent) {
         int damage = (int) (Math.random() * attack);
@@ -32,29 +54,60 @@ public class LivingThing {
         }
     }
 
+    /**
+     * hitPointのゲッター
+     * @return HP
+     */
+
     public int getHitPoint(){
         return hitPoint;
     }
+
+    /**
+     * hitPointのセッター
+     * @param hitPoint:HP
+     */
 
     public void setHitPoint(int hitPoint){
         this.hitPoint = hitPoint;
     }
 
+    /**
+     * attackのゲッター
+     * @return
+     */
     public int getAttack(){
         return attack;
     }
 
+    /**
+     * attckのセッター
+     * @param attack:与えたダメージ(攻撃力)
+     */
     public void setAttack(int attack){
         this.attack = attack;
     }
 
+    /**
+     * deadのゲッター
+     * @return
+     */
     public boolean getDead(){
         return dead;
     }
 
+    /**
+     * deadのセッター
+     * @param dead:生死状態
+     */
     public void setDead(boolean dead){
         this.dead = dead;
     }
+
+    /**
+     * 自分の合計ダメージを計算するメソッド
+     * @param damage:ダメージ
+     */
 
     public void wounded(int damage) {
         hitPoint -= damage;
